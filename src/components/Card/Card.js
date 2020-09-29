9851087302663a0c3f11480c9c0176e680b2fb8d
oliviamo <olivia.mo@bigtincan.com>
2020-09-29 22:43:02 +1000

    Add dayjs package to format date and add link to full image

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import dayjs from 'dayjs';
 import './Card.scss';
 
 const Card = ({
@@ -17,6 +18,8 @@ const Card = ({
       <img src={thumbnail} alt={title} />
       <div className="details-wrapper">
         <p>By {author.match(/"(.*?)"/)[1]}</p>
+        <p className="date">{dayjs(date).format('D MMMM YYYY')}</p>
+        <a href={fullImage} rel="noopener noreferrer" target="_blank">Link to full image</a>
       </div>
     </div>
   )
