5a0a021628e5b8af3f716c2be93c7a4e5c3664df
oliviamo <olivia.mo@bigtincan.com>
2020-10-01 00:04:55 +1000

    Move component to one line

--- src/components/FormattedDate/FormattedDate.js
+++ src/components/FormattedDate/FormattedDate.js
@@ -3,9 +3,7 @@ import PropTypes from 'prop-types';
 import dayjs from 'dayjs';
 import './FormattedDate.scss';
 
-const FormattedDate = ({ date }) => (
-  <p className="date">{dayjs(date).format('D MMMM YYYY')}</p>
-);
+const FormattedDate = ({ date }) => <p className="date">{dayjs(date).format('D MMMM YYYY')}</p>;
 
 FormattedDate.propTypes = {
   date: PropTypes.string.isRequired
--- src/components/FormattedText/FormattedText.js
+++ src/components/FormattedText/FormattedText.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const FormattedText = ({ text }) => (
-  <p>{text.match(/"(.*?)"/)[1]}</p>
-);
+const FormattedText = ({ text }) => <p>{text.match(/"(.*?)"/)[1]}</p>;
 
 FormattedText.defaultProps = {
   text: ''
